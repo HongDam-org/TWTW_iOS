64c5d29639d29933705f19b70f1d2e0932ec73d8
dami <dami8789@gmail.com>
2023-08-06 15:50:37 +0900

    feat:카카오로그인 사용자 정보불러오기

--- TWTW/TWTW/Services/SignInService.swift
+++ TWTW/TWTW/Services/SignInService.swift
@@ -27,6 +27,7 @@ class SignInService{
             UserApi.shared.rx.accessTokenInfo()
                 .subscribe(onSuccess: { (_) in
                     // 토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                    self.fetchKakaoUserInfo()
                     self.kakaoLoginSuccess.accept(()) // 토큰이 있으면 로그인 성공으로 처리
                 }, onFailure: { error in
                     if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true {
@@ -48,6 +49,7 @@ class SignInService{
                     .do(onNext: { _ in
                         print("loginWithKakaoAccount() success.")
                         // 로그인 성공 후 처리
+                        self.fetchKakaoUserInfo() // 로그인 성공 후 사용자 정보 불러오기
                         self.kakaoLoginSuccess.accept(()) // 로그인 성공으로 처리
                     }, onError: { error in
                         print("loginWithKakaoAccount() error: \(error)")
@@ -57,5 +59,30 @@ class SignInService{
             }
             .subscribe()
             .disposed(by: disposeBag)
+    }
+    
+    // 카카오 사용자 정보 불러오기
+    private func fetchKakaoUserInfo() {
+        UserApi.shared.rx.me()
+            .subscribe (onSuccess:{ user in
+                print("me() success.")
+                self.processKakaoUserInfo(user: user)
+                
+                _ = user
+            }, onFailure: {error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+    }
+    // 카카오 사용자 정보 처리
+    private func processKakaoUserInfo(user: KakaoSDKUser.User) {
+        // 사용자 정보를 받아오기
+        let userId = user.id
+        let userEmail = user.kakaoAccount?.email ?? ""
+        let userNickname = user.kakaoAccount?.profile?.nickname ?? ""
+        print(userId ?? "nil")
+        print(userEmail)
+        print(userNickname)
+
     }
 }
