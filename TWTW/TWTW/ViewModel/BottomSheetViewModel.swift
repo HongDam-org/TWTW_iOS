085f5e6ddce58f45e89a9f39bfffb77112f653b4
JJ <jeonghojin03@gmail.com>
2023-08-19 01:11:43 +0900

    refactor: change MVVM

--- TWTW/TWTW/ViewModel/BottomSheetViewModel.swift
+++ TWTW/TWTW/ViewModel/BottomSheetViewModel.swift
@@ -8,16 +8,21 @@
 import Foundation
 import RxSwift
 import RxCocoa
-import RxRelay
+import SnapKit
 
-///BottomSheetViewModel
 final class BottomSheetViewModel {
+    
+    var heightConstraintRelay: BehaviorRelay<Constraint?> = BehaviorRelay(value: nil)
+    
     var minHeight: CGFloat = 0.0
     var midHeight: CGFloat = 0.0
     var maxHeight: CGFloat = 0.0
     
     init(viewHeight: CGFloat) {
-        //setupHeight() -특정 세구간 높이 지정
+        setupHeight(viewHeight: viewHeight)
+    }
+    
+    private func setupHeight(viewHeight: CGFloat) {
         self.minHeight = viewHeight * 0.2
         self.midHeight = viewHeight * 0.5
         self.maxHeight = viewHeight * 0.8
@@ -25,11 +30,9 @@ final class BottomSheetViewModel {
     
     func calculateTargetHeight(currentHeight: CGFloat, translationY: CGFloat) -> CGFloat {
         let newHeight = currentHeight - translationY
-        let changedHeight = min(max(newHeight, minHeight * 0.8), maxHeight)
-        return changedHeight
+        return min(max(newHeight, minHeight * 0.8), maxHeight)
     }
     
-    ///특정 구간으로 지정
     func calculateFinalHeight(changedHeight: CGFloat) -> CGFloat {
         if changedHeight > midHeight {
             return maxHeight
@@ -38,5 +41,20 @@ final class BottomSheetViewModel {
         } else {
             return minHeight
         }
+    }
+    
+    func handlePan(gesture gestureRecognizer: UIPanGestureRecognizer, view: UIView) -> Observable<CGFloat> {
+        let translation = gestureRecognizer.translation(in: view)
+        let height = heightConstraintRelay.value?.layoutConstraints.first?.constant ?? CGFloat()
+
+        let changedHeight = calculateTargetHeight(currentHeight: height, translationY: translation.y)
+
+        heightConstraintRelay.accept(heightConstraintRelay.value?.update(offset: changedHeight))
+
+        let targetHeight = calculateFinalHeight(changedHeight: changedHeight)
+        
+        return gestureRecognizer.rx.event
+            .filter { $0.state == .ended || $0.state == .cancelled }
+            .map { _ in targetHeight }
     }
 }
