c15994411ed56f02b2231c77aa85fc12bf3c85e7
dami <dami8789@gmail.com>
2023-08-22 16:37:19 +0900

    feat:  add searchBar in MapView

---
+++ TWTW/TWTW/Views/SeachPlaces/SearchPlacesMapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchPlacesMapViewController.swift
+//  TWTW
+//
+//  Created by 박다미 on 2023/08/22.
+//
+
+import Foundation
+
+// 검색 결과를 표시하는 새로운 View Controller
+class SearchPlacesMapViewController: UIViewController {
+    // 검색 결과를 표시하는 로직을 구현할 수 있음
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
