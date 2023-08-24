c93313cd441acf4c3d9ae087391b272653ec0302
JJ <jeonghojin03@gmail.com>
2023-08-24 22:26:15 +0900

    fix: import issue

--- TWTW/TWTW/ViewModel/BottomSheetViewModel.swift
+++ TWTW/TWTW/ViewModel/BottomSheetViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
 import SnapKit
--- TWTW/TWTW/Views/SeachPlaces/SearchPlacesMapViewController.swift
+++ TWTW/TWTW/Views/SeachPlaces/SearchPlacesMapViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 // 검색 결과를 표시하는 새로운 View Controller
 class SearchPlacesMapViewController: UIViewController {
