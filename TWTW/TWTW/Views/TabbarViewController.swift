b194b944ebf5011384e216d9897b612695fc4969
dami <dami8789@gmail.com>
2023-09-10 19:20:20 +0900

    fix: change modle name before MVVM

--- TWTW/TWTW/Views/TabbarViewController.swift
+++ TWTW/TWTW/Views/TabbarViewController.swift
@@ -11,19 +11,19 @@ import RxSwift
 import RxCocoa
 import SnapKit
 
-final class TabBarController: UITabBarController {
-    // BehaviorRelay로 탭 아이템 저장
+class TabBarController: UITabBarController {
+    // 탭 아이템을 저장하는 BehaviorRelay
     private let tabItemsRelay = BehaviorRelay<[TabItem]>(value: [])
     weak var delegates: BottomSheetDelegate?
     
-    /// MainMapViewController view의 높이
+    // MainMapViewController 뷰의 높이를 나타내는 BehaviorRelay
     var viewHeight: BehaviorRelay<CGFloat> = BehaviorRelay(value: CGFloat())
     
-    private var bottomSheetViewModel = BottomSheetViewModel()
-    var isFirstLoad : Bool = true
+    private var tabBarViewModel = TabBarViewModel()
+    var isFirstLoad: Bool = true
     private let disposeBag = DisposeBag()
-    
-    // MARK: - init
+    let acceptableRange = 0.1
+    // 초기화 메서드
     init(delegates: BottomSheetDelegate? = nil, viewHeight: CGFloat) {
         super.init(nibName: nil, bundle: nil)
         
@@ -37,24 +37,25 @@ final class TabBarController: UITabBarController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - View Did Load
+    // 뷰가 로드된 후 호출되는 메서드
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.layer.cornerRadius = 20
         
-        // view의 높이를 설정하고
-            let viewHeight = self.view.bounds.height
-            // BottomSheetViewModel에 높이를 설정
-            bottomSheetViewModel.setupHeight(viewHeight: viewHeight)
+        // 뷰의 높이를 설정하고
+        let viewHeight = self.view.bounds.height
+        // BottomSheetViewModel에 높이를 설정
+        tabBarViewModel.setupHeight(viewHeight: viewHeight)
         print("🍎\(viewHeight)")
     }
     
+    // 뷰의 높이를 설정하는 메서드
     func setViewHeight(_ height: CGFloat) {
         viewHeight.accept(height)
     }
     
-    /// MARK: Tabbar와 VCs 연결
+    // 탭바와 뷰컨트롤러 연결
     private func setTabbar() {
         // 각 탭 아이템 생성
         let tabItems: [TabItem] = [
@@ -78,7 +79,7 @@ final class TabBarController: UITabBarController {
         bindTabItems()
     }
     
-    /// Tabbar 아이템을 BehaviorRelay를 사용하여 탭 바 아이템을 바인딩
+    // 탭 아이템을 BehaviorRelay를 사용하여 탭 바 아이템에 연결
     private func bindTabItems() {
         tabItemsRelay
             .asObservable()
@@ -93,70 +94,65 @@ final class TabBarController: UITabBarController {
             .disposed(by: disposeBag)
     }
     
-    /// Add UI
+    // 뷰 컨트롤러에 서브뷰 추가
     private func addSubViews() {
         viewControllers?.forEach { viewController in
             let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
             viewController.view.addGestureRecognizer(panGesture)
-            bottomSheetViewModel.setupHeight(viewHeight: viewHeight.value)
+            tabBarViewModel.setupHeight(viewHeight: viewHeight.value)
         }
     }
     
+    // 팬 제스처 핸들링 메서드
+    
     @objc private func handlePan(_ panGesture: UIPanGestureRecognizer) {
-        // 현재 손가락 터치 위치 출력
-        let location = panGesture.location(in: view)
-        // 패닝 제스처의 변화
-        let translation = panGesture.translation(in: view)
-        // 뷰 컨트롤러의 현재 높이
-        var currentHeight = viewHeight.value
-        // 패닝 제스처의 변화를 기반으로 새로운 높이를 계산
-        let targetHeight = currentHeight - translation.y
-        // 새로운 높이로 뷰 컨트롤러의 높이를 업데이트
-        updateBottomSheetHeight(targetHeight)
-        // 패닝 제스처의 변화를 초기화.
-        panGesture.setTranslation(.zero, in: view)
+        switch panGesture.state {
+        case .began:
+            // 초기 터치 위치
+            tabBarViewModel.initialTouchY = panGesture.location(in: view).y
+        case .changed:
+            let lastTouchY = panGesture.location(in: view).y
+            let gapTouchY = tabBarViewModel.initialTouchY - lastTouchY // Y 좌표 간격을 계산
 
-        // 손가락을 떼었을 때 최소(min), 중간(mid), 최대(max)로 이동
-        if panGesture.state == .ended || panGesture.state == .cancelled {
-            let finalHeight: CGFloat
-            if targetHeight > bottomSheetViewModel.midHeight {
-                finalHeight = bottomSheetViewModel.maxHeight
-            } else if targetHeight > bottomSheetViewModel.minHeight {
-                finalHeight = bottomSheetViewModel.midHeight
-            } else {
-                finalHeight = bottomSheetViewModel.minHeight
-            }
+            // 뷰의 현재 높이와 이동된 간격을 기반으로 새로운 높이를 계산
+            var heightByTouch = viewHeight.value + gapTouchY
 
-            // 최종 높이로 애니메이션을 사용하여 높이를 부드럽게 업데이트
-            UIView.animate(withDuration: 0.3) {
+            // 최소 높이와 최대 높이를 벗어나지 않도록 보정
+            heightByTouch = min(max(heightByTouch, tabBarViewModel.minHeight * (1 - acceptableRange)), tabBarViewModel.maxHeight * (1 + acceptableRange))
+
+            updateBottomSheetHeight(heightByTouch)
+        case .ended, .cancelled:
+            let targetHeight = tabBarViewModel.calculateFinalHeight(changedHeight: viewHeight.value)
+
+            // ViewModel을 사용하여 최종 높이를 계산
+            let finalHeight = tabBarViewModel.calculateFinalHeight(changedHeight: targetHeight)
+
+            UIView.animate(withDuration: 0.1) {
                 self.updateBottomSheetHeight(finalHeight)
                 self.view.layoutIfNeeded()
             }
 
-            // 최종 높이를 델리게이트에 알림
             delegates?.didUpdateBottomSheetHeight(finalHeight)
+        default:
+            break
         }
+    
     }
-
+    // 뷰의 높이 업데이트
     func updateBottomSheetHeight(_ height: CGFloat) {
-
         var newHeight = height
         if isFirstLoad {
-            if newHeight > bottomSheetViewModel.maxHeight {
-                newHeight = bottomSheetViewModel.minHeight
+            if newHeight > tabBarViewModel.maxHeight {
+                newHeight = tabBarViewModel.minHeight
                 isFirstLoad = false
             }
-            isFirstLoad = false
         }
-           viewHeight.accept(newHeight)
-           bottomSheetViewModel.heightConstraintRelay.accept(bottomSheetViewModel.heightConstraintRelay.value?.update(offset: newHeight))
-           delegates?.didUpdateBottomSheetHeight(newHeight)
+        viewHeight.accept(newHeight)
+        tabBarViewModel.heightConstraintRelay.accept(tabBarViewModel.heightConstraintRelay.value?.update(offset: newHeight))
+        delegates?.didUpdateBottomSheetHeight(newHeight)
     }
 }
-
-// 탭 아이템 나타내는 구조체
 struct TabItem {
     let title: String
     let imageName: String
 }
-
