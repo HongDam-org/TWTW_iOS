0faf2bbe35221a1bfb80c7a833acdbb6c64ea75f
dami <dami0804@naver.com>
2023-11-23 14:43:24 +0900

    test: searchPlace result비교수정

--- TWTW/TWTWServiceUnitTests/SearchPlaces/SearchPlaceMapViewModelUnitTests.swift
+++ TWTW/TWTWServiceUnitTests/SearchPlaces/SearchPlaceMapViewModelUnitTests.swift
@@ -12,14 +12,14 @@ import RxCocoa
 @testable import TWTW
 
 final class SearchPlacesMapViewModelUnitTests: XCTestCase {
-    
+
     private var disposeBag: DisposeBag!
     private var scheduler: TestScheduler!
     private var viewModel: SearchPlacesMapViewModel!
     private var mockService: MockSearchPlacesMapService!
     private var input: SearchPlacesMapViewModel.Input!
     private var output: SearchPlacesMapViewModel.Output!
-    
+
     override func setUpWithError() throws {
         try super.setUpWithError()
         scheduler = TestScheduler(initialClock: 0)
@@ -27,48 +27,50 @@ final class SearchPlacesMapViewModelUnitTests: XCTestCase {
         viewModel = SearchPlacesMapViewModel(coordinator: nil, searchPlacesServices: mockService)
         disposeBag = DisposeBag()
     }
-    
+
     override func tearDownWithError() throws {
         disposeBag = nil
         scheduler = nil
         viewModel = nil
         try super.tearDownWithError()
     }
+
     func testLoadDataOnSearchTextChange() {
-        // Mock response 설정
-        let mockPlaces =
-        [SearchPlace(placeName: "Place1", distance: "100m", placeURL: "url", categoryName: "Cafe",
-                     addressName: "Address", roadAddressName: "Road Address", categoryGroupCode: "CGC", xPosition: "100.0", yPosition: "200.0")]
-        mockService.mockResponse = PlaceResponse(results: mockPlaces, isLast: false)
-        
+        let mockPlace = SearchPlace(placeName: "Place1", distance: "100m", placeURL: "url", categoryName: "Cafe",
+                                    addressName: "Address", roadAddressName: "Road Address", categoryGroupCode: "CGC", xPosition: "100.0", yPosition: "200.0")
+        mockService.mockResponse = PlaceResponse(results: [mockPlace], isLast: false)
+
         let searchTextObservable = scheduler.createHotObservable([
-            .next(10, "test query")
+            .next(10, "Place1")
         ]).asObservable().map { $0 as String? }
-        
+
         input = SearchPlacesMapViewModel.Input(
             searchText: searchTextObservable,
             loadMoreTrigger: PublishRelay<Void>(),
             selectedCoorinate: Observable<SearchPlace>.never()
         )
-        
+
         output = viewModel.bind(input: input)
-        
+
         let result = scheduler.createObserver([SearchPlace].self)
         output.filteredPlaces.bind(to: result).disposed(by: disposeBag)
-        
+
         scheduler.start()
-       
-        XCTAssertEqual(result.events.count, 2)
-        //           if let firstEvent = result.events.first {
-        //               //XCTAssertEqual(firstEvent.time, 10)
-        //              // XCTAssertNotNil(firstEvent.value.element)
-        //           }
+
+        if let resultAt10 = result.events.filter({ $0.time == 10 })
+            .first?.value.element?.first {
+            XCTAssertEqual(resultAt10.placeName, mockPlace.placeName, "time 10에 place1결과")
+        } else {
+            XCTFail("결과없음")
+        }
     }
+
     func testSelectingPlaceCallsCoordinator() {
         // Mock response
         let selectedPlace =
         SearchPlace(placeName: "Place1", distance: "100m", placeURL: "url", categoryName: "Cafe",
-                    addressName: "Address", roadAddressName: "Road Address", categoryGroupCode: "CGC", xPosition: "100.0", yPosition: "200.0")
+                    addressName: "Address", roadAddressName: "Road Address", categoryGroupCode: "CGC",
+                    xPosition: "100.0", yPosition: "200.0")
         let selectedPlaceObservable = scheduler.createHotObservable([
             .next(15, selectedPlace)
         ]).asObservable()
@@ -79,7 +81,7 @@ final class SearchPlacesMapViewModelUnitTests: XCTestCase {
         )
         output = viewModel.bind(input: input)
         scheduler.start()
-        
+
         // MockSearchPlacesMapCoordinator, finishSearchPlaces 호출 여부
         // XCTAssertTrue(mockCoordinator.finishSearchPlacesCalled)
     }
