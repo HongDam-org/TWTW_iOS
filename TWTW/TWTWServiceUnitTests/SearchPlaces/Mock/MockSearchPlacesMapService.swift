b28c4f4d7ac132bcdab5731532063b2153337db8
dami <dami0804@naver.com>
2023-11-25 16:07:06 +0900

    test: 단순화

--- TWTW/TWTWServiceUnitTests/SearchPlaces/Mock/MockSearchPlacesMapService.swift
+++ TWTW/TWTWServiceUnitTests/SearchPlaces/Mock/MockSearchPlacesMapService.swift
@@ -15,32 +15,30 @@ final class MockSearchPlacesMapService: SearchPlaceProtocol {
     var mockPlace2 = SearchPlace(placeName: "Place2", distance: "200m", placeURL: "url2",
                                  categoryName: "Cafe2", addressName: "Address2", roadAddressName: "RoadAdd2",
                                  categoryGroupCode: "CGC2", xPosition: "300.0", yPosition: "400.0")
-    
+
     func searchPlaceService(request: PlacesRequest) -> Observable<PlaceResponse> {
-        return Observable.create { [weak self] observer in
             if request.searchText == "Place1"{
-                let response = PlaceResponse(results: [self?.mockPlace1].compactMap { $0 }, isLast: false)
-                observer.onNext(response)
-            } else if request.searchText == "Place2"{
-                let response = PlaceResponse(results: [self?.mockPlace2].compactMap { $0 }, isLast: false)
-                observer.onNext(response)
-            } else if request.searchText == "Place" {
+                let response = PlaceResponse(results: [mockPlace1].compactMap { $0 }, isLast: false)
+                return .just(response)
+            } 
+            else if request.searchText == "Place2"{
+                let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: false)
+                return .just(response)
+            } 
+            else if request.searchText == "Place" {
                 switch request.pageNum {
                 case 1:
-                    let response = PlaceResponse(results: [self?.mockPlace1].compactMap { $0 }, isLast: false)
-                    observer.onNext(response)
+                    let response = PlaceResponse(results: [mockPlace1].compactMap { $0 }, isLast: false)
+                    return .just(response)
                 case 2:
-                    let response = PlaceResponse(results: [self?.mockPlace2].compactMap { $0 }, isLast: true)
-                    observer.onNext(response)
+                    let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: true)
+                    return .just(response)
                 default:
-                    observer.onError(NSError(domain: "unitTest", code: -1, userInfo: nil))
+                    return .error(NSError(domain: "unitTest", code: -1, userInfo: nil))
                 }
-            }
-            
+            } 
             else {
-                observer.onError(NSError(domain: "unitTest", code: -1, userInfo: nil))
+                return .error(NSError(domain: "unitTest", code: -1, userInfo: nil))
             }
-            return Disposables.create()
-        }
     }
 }
