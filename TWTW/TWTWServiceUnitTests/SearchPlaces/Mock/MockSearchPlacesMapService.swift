2e2830b45020b97e1489cdec364b9004d3da4ed8
HJ <81843677+HJ39@users.noreply.github.com>
2023-11-28 17:45:25 +0900

    hotfix: merge error

--- TWTW/TWTWServiceUnitTests/SearchPlaces/Mock/MockSearchPlacesMapService.swift
+++ TWTW/TWTWServiceUnitTests/SearchPlaces/Mock/MockSearchPlacesMapService.swift
@@ -9,36 +9,32 @@ import Foundation
 import RxSwift
 
 final class MockSearchPlacesMapService: SearchPlaceProtocol {
-    var mockPlace1 = SearchPlace(placeName: "Place1", distance: "100m", placeURL: "url",
+    var mockPlace1 = SearchPlace(placeName: "Place1", distance: 100, placeURL: "url",
                                  categoryName: "Cafe", addressName: "Address", roadAddressName: "RoadAdd",
-                                 categoryGroupCode: "CGC", xPosition: "100.0", yPosition: "200.0")
-    var mockPlace2 = SearchPlace(placeName: "Place2", distance: "200m", placeURL: "url2",
+                                 categoryGroupCode: "CGC", xPosition: 100.0, yPosition: 200.0)
+    var mockPlace2 = SearchPlace(placeName: "Place2", distance: 200, placeURL: "url2",
                                  categoryName: "Cafe2", addressName: "Address2", roadAddressName: "RoadAdd2",
-                                 categoryGroupCode: "CGC2", xPosition: "300.0", yPosition: "400.0")
+                                 categoryGroupCode: "CGC2", xPosition: 300.0, yPosition: 400.0)
 
     func searchPlaceService(request: PlacesRequest) -> Observable<PlaceResponse> {
-            if request.searchText == "Place1"{
+        if request.searchText == "Place1" {
+            let response = PlaceResponse(results: [mockPlace1].compactMap { $0 }, isLast: false)
+            return .just(response)
+        } else if request.searchText == "Place2" {
+            let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: false)
+            return .just(response)
+        } else if request.searchText == "Place" {
+            switch request.pageNum {
+            case 1:
                 let response = PlaceResponse(results: [mockPlace1].compactMap { $0 }, isLast: false)
                 return .just(response)
-            } 
-            else if request.searchText == "Place2"{
-                let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: false)
+            case 2:
+                let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: true)
                 return .just(response)
-            } 
-            else if request.searchText == "Place" {
-                switch request.pageNum {
-                case 1:
-                    let response = PlaceResponse(results: [mockPlace1].compactMap { $0 }, isLast: false)
-                    return .just(response)
-                case 2:
-                    let response = PlaceResponse(results: [mockPlace2].compactMap { $0 }, isLast: true)
-                    return .just(response)
-                default:
-                    return .error(NSError(domain: "unitTest", code: -1, userInfo: nil))
-                }
-            } 
-            else {
+            default:
                 return .error(NSError(domain: "unitTest", code: -1, userInfo: nil))
             }
+        }
+        return .error(NSError(domain: "unitTest", code: -1, userInfo: nil))
     }
 }
