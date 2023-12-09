3ad9d22cc12ed12de9df4cf394e89d92d244719a
dami <dami0804@naver.com>
2023-12-09 20:22:01 +0900

    feat: 탭바 구조 수정 및 navi Item위치 수정

--- TWTW/TWTW/Tabbar/DefaultTabBarCoordinator.swift
+++ TWTW/TWTW/Tabbar/DefaultTabBarCoordinator.swift
@@ -14,43 +14,33 @@ final class DefaultTabBarCoordinator: TabBarCoordinator {
     var navigationController: UINavigationController
     var tabBarController: UITabBarController
     
-    /// 탭버튼 코디네이터 4개
-    var defaultGroupCoordinator: DefaultGroupCoordinator
-    var defaultFriendsListCoordinator: DefaultFriendsListCoordinator
-    var defaultNotificationCoordinator: DefaultNotificationCoordinator
-    var defaultMyPageCoordinator: DefaultMyPageCoordinator
+    var tabCoordinators: [TabCoordinator]
     
+    // MARK: - Init
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
         self.tabBarController = UITabBarController()
         
-        /// 각 탭 코디네이터 초기화
-        self.defaultGroupCoordinator = DefaultGroupCoordinator(navigationController: UINavigationController())
-        self.defaultFriendsListCoordinator = DefaultFriendsListCoordinator(navigationController: UINavigationController())
-        self.defaultNotificationCoordinator = DefaultNotificationCoordinator(navigationController: UINavigationController())
-        self.defaultMyPageCoordinator = DefaultMyPageCoordinator(navigationController: UINavigationController())
-        
-        childCoordinators.append(contentsOf: [
-            defaultGroupCoordinator as Coordinator,
-            defaultFriendsListCoordinator as Coordinator,
-            defaultNotificationCoordinator as Coordinator,
-            defaultMyPageCoordinator as Coordinator
-        ])
-        
+        // 각 탭 코디네이터 초기화
+               tabCoordinators = [
+                   TabCoordinator(title: "홈", icon: UIImage(systemName: "house"),
+                                  coordinator: DefaultGroupCoordinator(navigationController: UINavigationController())),
+                   TabCoordinator(title: "친구목록", icon: UIImage(systemName: "person.2"),
+                                  coordinator: DefaultFriendsListCoordinator(navigationController: UINavigationController())),
+                   TabCoordinator(title: "알림", icon: UIImage(systemName: "bell"),
+                                  coordinator: DefaultNotificationCoordinator(navigationController: UINavigationController())),
+                   TabCoordinator(title: "마이페이지", icon: UIImage(systemName: "person"),
+                                  coordinator: DefaultMyPageCoordinator(navigationController: UINavigationController()))
+               ]
+        childCoordinators.append(contentsOf: tabCoordinators.map { $0.coordinator })
     }
     
     func start() {
-        childCoordinators.forEach { $0.start() }
-        
-        let groupNavigationController = defaultGroupCoordinator.navigationController
-        groupNavigationController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "person"), selectedImage: nil)
-        
-        let friendsListNavgationController = defaultFriendsListCoordinator.navigationController
-        friendsListNavgationController.tabBarItem = UITabBarItem(title: "친구목록", image: UIImage(systemName: "person"), selectedImage: nil)
-        let notificationNavgationController = defaultNotificationCoordinator.navigationController
-        notificationNavgationController.tabBarItem = UITabBarItem(title: "알림", image: UIImage(systemName: "person"), selectedImage: nil)
-        let myPageNavgationController = defaultMyPageCoordinator.navigationController
-        myPageNavgationController.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person"), selectedImage: nil)
+        tabCoordinators.forEach { tabCoordinator in
+            let navigationController = tabCoordinator.coordinator.navigationController
+            navigationController.tabBarItem = UITabBarItem(title: tabCoordinator.title, image: tabCoordinator.icon, selectedImage: nil)
+            tabCoordinator.coordinator.start()
+        }
         
         tabBarController.viewControllers = childCoordinators.map { $0.navigationController }
         navigationController.viewControllers = [tabBarController]
---
+++ TWTW/TWTW/Tabbar/TabCoordinator.swift
@@ -0,0 +1,15 @@
+//
+//  TabCoordinator.swift
+//  TWTW
+//
+//  Created by 박다미 on 2023/12/09.
+//
+
+import UIKit
+
+/// 탭 코디네이터 구조체
+struct TabCoordinator {
+    let title: String
+    let icon: UIImage?
+    let coordinator: Coordinator
+}
