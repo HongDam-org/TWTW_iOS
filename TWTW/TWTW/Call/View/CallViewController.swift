c870b7706d267f29fd373f0d140f8a9cecabdaea
HJ <81843677+HJ39@users.noreply.github.com>
2023-12-11 12:47:37 +0900

    feat: set call

--- TWTW/TWTW/Call/View/CallViewController.swift
+++ TWTW/TWTW/Call/View/CallViewController.swift
@@ -5,16 +5,33 @@
 //  Created by 박다미 on 2023/08/26.
 //
 
+import CallKit
 import Foundation
 import UIKit
 
 // 통화하기
 final class CallViewController: UIViewController {
-
-    // MARK: View Did Load
+    let cv = CXCallController()
+    
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.layer.cornerRadius = 20
+    }
+}
+
+
+extension CallViewController: CXProviderDelegate {
+    func providerDidReset(_ provider: CXProvider) {
+        
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+        action.fulfill()
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        action.fulfill()
     }
 }
