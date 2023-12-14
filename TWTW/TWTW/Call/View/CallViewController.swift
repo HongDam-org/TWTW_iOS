b71ae77baca2ec706e996631c38379239697fc76
HJ <81843677+HJ39@users.noreply.github.com>
2023-12-15 02:22:13 +0900

    feat: Setting Calling Function

--- TWTW/TWTW/Call/View/CallViewController.swift
+++ TWTW/TWTW/Call/View/CallViewController.swift
@@ -7,20 +7,48 @@
 
 import CallKit
 import Foundation
+import PushKit
 import UIKit
 
-// 통화하기
+/// 통화하기
 final class CallViewController: UIViewController {
-    let cv = CXCallController()
-    
+    private let cxProvider = CXProvider(configuration: CXProviderConfiguration())
+    let callController = CXCallController()
     
     // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        registry()
+        
     }
+    
+    /// 전화 하기
+    private func outgoing() {
+        let uuid = UUID()
+        let handle = CXHandle(type: .phoneNumber, value: "01041007930")
+        let startCallAction = CXStartCallAction(call: uuid, handle: handle)
+        let transaction = CXTransaction(action: startCallAction)
+        
+        print(#function, uuid, handle)
+        callController.request(transaction) { error in
+            print(error ?? "")
+            print("Start Call")
+        }
+        
+        DispatchQueue.main.asyncAfter(wallDeadline: DispatchWallTime.now() + 5) {
+            self.cxProvider.reportOutgoingCall(with: self.callController.callObserver.calls[0].uuid, connectedAt: nil)
+        }
+    }
+    
+    /// registry
+    private func registry() {
+        let registry = PKPushRegistry(queue: nil)
+        registry.delegate = self
+        registry.desiredPushTypes = [PKPushType.voIP]
+    }
+    
 }
-
 
 extension CallViewController: CXProviderDelegate {
     func providerDidReset(_ provider: CXProvider) {
@@ -33,5 +61,31 @@ extension CallViewController: CXProviderDelegate {
     
     func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
         action.fulfill()
+    }
+    
+    // 전화 걸기 델리게이트 메소드
+    func provider(_ provider: CXProvider, perform action: CXStartCallAction) {
+        print(#function, provider, action)
+        action.fulfill()
+    }
+}
+
+
+extension CallViewController: PKPushRegistryDelegate {
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        let deviceID = pushCredentials.token.map { String(format: "%02.2hhx", $0) }.joined()
+        print(#function, deviceID)
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        let config = CXProviderConfiguration()
+        config.includesCallsInRecents = false
+        config.supportsVideo = true;
+        let provider = CXProvider(configuration: config)
+        provider.setDelegate(self, queue: nil)
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .generic, value: "Fomagran")
+        update.hasVideo = true
+        provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
     }
 }
