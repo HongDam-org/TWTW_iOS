a7ad802e28658b4fbc5d185bade25e757faf9a41
dami <dami0804@naver.com>
2023-12-21 15:58:10 +0900

    feat: plansFromAlertViewController UI

--- TWTW/TWTW/Plan/FromAlert/PlansFromAlertViewController.swift
+++ TWTW/TWTW/Plan/FromAlert/PlansFromAlertViewController.swift
@@ -14,7 +14,7 @@ final class PlansFromAlertViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private var viewModel: PlansFromAlertViewModel
     private let datePickerViewController = DatePickerViewController()
-
+    
     private lazy var selectedFriendsTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(FriendListTableViewCell.self, forCellReuseIdentifier: CellIdentifier.friendListTableViewCell.rawValue)
@@ -22,20 +22,42 @@ final class PlansFromAlertViewController: UIViewController {
         tableView.isScrollEnabled = false
         return tableView
     }()
+    
     private lazy var originalMeetingNameLabel: UILabel = {
         let label = UILabel()
-        label.text = "약속 명 (수정가능)"
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        label.text = "약속 명"
         return label
     }()
-    
+    private lazy var meetingNameEditButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "pencil"), for: .normal)
+        button.tintColor = .gray
+        return button
+    }()
+    // place이전 이후
+    private lazy var placeNameStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
     private lazy var originalPlaceNameLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
         label.text = "이전 목적지 명"
         return label
+    }()
+    private lazy var placeChangeImage: UIImageView = {
+        let image = UIImageView(image: UIImage(systemName: "arrowshape.right"))
+        image.tintColor = .black
+        return image
     }()
     
     private lazy var newPlaceNameLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         label.text = "선택한 목적지명"
         
         return label
@@ -43,10 +65,12 @@ final class PlansFromAlertViewController: UIViewController {
     
     private lazy var addParticipantsButton: UIButton = {
         let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "person.2.circle"), for: .normal)
         button.setTitle("참여 인원", for: .normal)
-        
+        button.tintColor = .black
         return button
     }()
+    
     private lazy var confirmUIView: UIView = {
         let uiView = UIView()
         uiView.backgroundColor = .white
@@ -62,19 +86,21 @@ final class PlansFromAlertViewController: UIViewController {
     
     private lazy var datePickerButton: UIButton = {
         let button = UIButton(type: .system)
+        button.setTitle("날짜설정", for: .normal)
         button.setImage(UIImage(systemName: "calendar"), for: .normal)
+        button.tintColor = .gray
         return button
     }()
     
     private lazy var selectedDateLabel: UILabel = {
         let label = UILabel()
-        label.text = "선택한 날짜"
+        label.text = "날짜를 선택해주세요"
         
         return label
     }()
     private lazy var selectedTimeLabel: UILabel = {
         let label = UILabel()
-        label.text = "선택한 시간"
+        label.text = "시간을 선택해주세요"
         
         return label
     }()
@@ -85,8 +111,8 @@ final class PlansFromAlertViewController: UIViewController {
         picker.datePickerMode = .date
         return picker
     }()
-    let scrollView = UIScrollView()
-    let contentView = UIView()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     
     // MARK: - Init
     init(viewModel: PlansFromAlertViewModel) {
@@ -103,7 +129,6 @@ final class PlansFromAlertViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         addSubeViews()
-        setupBindings()
         bind()
         bindTableView()
         
@@ -114,10 +139,18 @@ final class PlansFromAlertViewController: UIViewController {
         view.addSubview(confirmUIView)
         confirmUIView.addSubview(confirmButton)
         scrollView.addSubview(contentView)
+        
+        contentView.addSubview(originalMeetingNameLabel)
+        contentView.addSubview(meetingNameEditButton)
+        
+        contentView.addSubview(placeNameStackView)
+        placeNameStackView.addArrangedSubview(originalPlaceNameLabel)
+        placeNameStackView.addArrangedSubview(placeChangeImage)
+        placeNameStackView.addArrangedSubview(newPlaceNameLabel)
         // 원래 위치 레이블
-        contentView.addSubview(originalPlaceNameLabel)
-        // 새 위치 레이블
-        contentView.addSubview(newPlaceNameLabel)
+        //        contentView.addSubview(originalPlaceNameLabel)
+        //        // 새 위치 레이블
+        //        contentView.addSubview(newPlaceNameLabel)
         // 참여 인원 추가 버튼
         contentView.addSubview(addParticipantsButton)
         
@@ -134,45 +167,63 @@ final class PlansFromAlertViewController: UIViewController {
     
     private func configureConstraints() {
         scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-            
+            make.top.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(confirmUIView.snp.top).offset(10)
         }
         
         contentView.snp.makeConstraints { make in
             make.edges.equalTo(scrollView)
             make.width.equalTo(scrollView)
         }
-        
-        originalPlaceNameLabel.snp.makeConstraints { make in
+        // 약속명
+        originalMeetingNameLabel.snp.makeConstraints { make in
             make.top.equalTo(contentView.snp.top).offset(20)
             make.centerX.equalToSuperview()
         }
-        newPlaceNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(originalPlaceNameLabel.snp.bottom).offset(10)
+        meetingNameEditButton.snp.makeConstraints { make in
+            make.centerY.equalTo(originalMeetingNameLabel)
+            make.leading.equalTo(originalMeetingNameLabel.snp.trailing).offset(5)
+        }
+        // 목적지
+        placeNameStackView.snp.makeConstraints { make in
+            make.top.equalTo(originalMeetingNameLabel.snp.bottom).offset(20)
             make.centerX.equalToSuperview()
         }
-        addParticipantsButton.snp.makeConstraints { make in
-            make.top.equalTo(newPlaceNameLabel.snp.bottom).offset(10)
-            make.centerX.equalToSuperview()
-        }
-        selectedFriendsTableView.snp.makeConstraints { make in
-            make.top.equalTo(addParticipantsButton.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(0)
-        }
+        //        originalPlaceNameLabel.snp.makeConstraints { make in
+        //            make.top.equalTo(originalMeetingNameLabel.snp.bottom).offset(10)
+        //            make.centerX.equalToSuperview()
+        //        }
+        //        newPlaceNameLabel.snp.makeConstraints { make in
+        //            make.top.equalTo(originalPlaceNameLabel.snp.bottom).offset(10)
+        //            make.centerX.equalToSuperview()
+        //        }
+        // 달력버튼
         datePickerButton.snp.makeConstraints { make in
-            make.top.equalTo(selectedFriendsTableView.snp.bottom).offset(10)
+            make.top.equalTo(placeNameStackView.snp.bottom).offset(10)
             make.centerX.equalToSuperview()
         }
+        // 날짜 라벨
         selectedDateLabel.snp.makeConstraints { make in
             make.top.equalTo(datePickerButton.snp.bottom).offset(10)
             make.centerX.equalToSuperview()
         }
+        // 시간 라벨
         selectedTimeLabel.snp.makeConstraints { make in
             make.top.equalTo(selectedDateLabel.snp.bottom).offset(10)
             make.centerX.equalToSuperview()
+        }
+        // 참여 인원 추가 버튼
+        addParticipantsButton.snp.makeConstraints { make in
+            make.top.equalTo(selectedTimeLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        selectedFriendsTableView.snp.makeConstraints { make in
+            make.top.equalTo(addParticipantsButton.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(0)
             make.bottom.equalToSuperview().inset(20)
         }
+        
         confirmUIView.snp.makeConstraints { make in
             make.horizontalEdges.equalToSuperview()
             make.bottom.equalToSuperview()
@@ -204,6 +255,23 @@ final class PlansFromAlertViewController: UIViewController {
                        cellType: FriendListTableViewCell.self)) { index, friend, cell in
                 cell.inputData(info: friend)
             }.disposed(by: disposeBag)
+        
+        
+        // alert 창 터치 이벤트 vc에서 진행함.
+        
+        datePickerButton.rx.tap
+            .bind { [weak self] in
+                self?.presentDatePicker()
+            }
+            .disposed(by: disposeBag)
+        
+        
+        meetingNameEditButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.showEditAlert()
+            })
+            .disposed(by: disposeBag)
+        
     }
     
     private func updateTableViewHeight(_ count: Int) {
@@ -214,16 +282,28 @@ final class PlansFromAlertViewController: UIViewController {
         }
         view.layoutIfNeeded()
     }
-    private func setupBindings() {
-        datePickerButton.rx.tap
-            .bind { [weak self] in
-                self?.presentDatePicker()
+    
+    
+    private func showEditAlert() {
+        let alertController = UIAlertController(title: "약속 명 변경", message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "새 약속 명을 입력하세요"
+        }
+        let confirmAction = UIAlertAction(title: "확인", style: .default) { [weak self, unowned alertController] _ in
+            if let newName = alertController.textFields?.first?.text, !newName.isEmpty {
+                self?.originalMeetingNameLabel.text = newName
             }
-            .disposed(by: disposeBag)
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
     }
+    
+    
     private func presentDatePicker() {
         datePickerViewController.modalPresentationStyle = .formSheet
-        datePickerViewController.modalTransitionStyle = .flipHorizontal
+        datePickerViewController.modalTransitionStyle = .coverVertical
         
         datePickerViewController.selectedDate
             .subscribe(onNext: { [weak self] selectedDate in
